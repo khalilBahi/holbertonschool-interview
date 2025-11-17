74ba7a67141a619e48e2627c4bb9ed9e7da450a5
khalilBahi <7753@holbertonstudents.com>
2025-11-17 14:30:17 +0100

    function that inserts a value into a Max Binary Heap

--- heap_insert/1-heap_insert.c
+++ heap_insert/1-heap_insert.c
@@ -1,84 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "binary_trees.h"
 
 /**
- * heap_size - Measures the size of a binary tree
- * @tree: Pointer to the root node of the tree to measure the size
+ * heap_insert - Inserts a value into a Max Binary Heap.
+ * @root: Double pointer to the root node of the Heap.
+ * @value: The value to insert in the heap.
  *
- * Return: Size of the tree, or 0 if tree is NULL
- */
-size_t heap_size(const binary_tree_t *tree)
-{
-	if (tree == NULL)
-		return (0);
-
-	return (1 + heap_size(tree->left) + heap_size(tree->right));
-}
-
-/**
- * find_last_level_parent - Find parent for insertion using level order
- * @root: Root of the heap
- *
- * Return: Parent node where next insertion should happen
- */
-heap_t *find_last_level_parent(heap_t *root)
-{
-	heap_t *queue[1024];
-	int front = 0, rear = 0;
-	heap_t *current;
-
-	if (!root)
-		return (NULL);
-
-	queue[rear++] = root;
-
-	while (front < rear)
-	{
-		current = queue[front++];
-
-		if (!current->left || !current->right)
-			return (current);
-
-		queue[rear++] = current->left;
-		queue[rear++] = current->right;
-	}
-
-	return (NULL);
-}
-
-/**
- * heapify_up - Maintains max heap property by bubbling up
- * @node: Pointer to the node to heapify up
- *
- * Return: Pointer to the final position of the node
- */
-heap_t *heapify_up(heap_t *node)
-{
-	int temp;
-	heap_t *original_node = node;
-
-	while (node->parent && node->n > node->parent->n)
-	{
-		temp = node->n;
-		node->n = node->parent->n;
-		node->parent->n = temp;
-		node = node->parent;
-	}
-
-	return (original_node);
-}
-
-/**
- * heap_insert - Inserts a value into a Max Binary Heap
- * @root: Double pointer to the root node of the Heap
- * @value: Value to store in the node to be inserted
- *
- * Return: Pointer to the inserted node, or NULL on failure
+ * Return: Pointer to the newly inserted node, or NULL on failure.
  */
 heap_t *heap_insert(heap_t **root, int value)
 {
 	heap_t *new_node, *parent;
+	size_t size, index;
 
-	if (root == NULL)
+	if (!root)
 		return (NULL);
 
 	if (*root == NULL)
@@ -87,15 +23,81 @@ heap_t *heap_insert(heap_t **root, int value)
 		return (*root);
 	}
 
-	parent = find_last_level_parent(*root);
-	new_node = binary_tree_node(parent, value);
-	if (new_node == NULL)
-		return (NULL);
+	/* Find the correct position to insert */
+	size = binary_tree_size(*root);
+	index = binary_tree_find_index(size + 1);
+	parent = get_parent_node(*root, index);
 
+	/* Create and attach the new node */
+	new_node = binary_tree_node(parent, value);
 	if (parent->left == NULL)
 		parent->left = new_node;
 	else
 		parent->right = new_node;
 
-	return (heapify_up(new_node));
+	/* Reheapify (bubble up) */
+	while (new_node->parent && new_node->n > new_node->parent->n)
+	{
+		swap_values(new_node, new_node->parent);
+		new_node = new_node->parent;
+	}
+
+	return (new_node);
+}
+
+/**
+ * binary_tree_size - Measures the size of a binary tree.
+ * @tree: Pointer to the root node of the tree.
+ *
+ * Return: Size of the tree (number of nodes).
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+}
+
+/**
+ * binary_tree_find_index - Calculates the position index for a new node.
+ * @size: Size of the tree + 1.
+ *
+ * Return: Index of the new node in a level-order traversal.
+ */
+size_t binary_tree_find_index(size_t size)
+{
+	return (size);
+}
+
+/**
+ * get_parent_node - Finds the parent node for a new node based on index.
+ * @root: Pointer to the root node of the tree.
+ * @index: Index of the new node in level-order.
+ *
+ * Return: Pointer to the parent node.
+ */
+heap_t *get_parent_node(heap_t *root, size_t index)
+{
+	heap_t *node = root;
+	size_t mask = 1 << (sizeof(size_t) * 8 - __builtin_clz(index) - 2);
+
+	while (mask > 1)
+	{
+		node = (index & mask) ? node->right : node->left;
+		mask >>= 1;
+	}
+	return (node);
+}
+
+/**
+ * swap_values - Swaps the values of two nodes.
+ * @a: First node.
+ * @b: Second node.
+ */
+void swap_values(heap_t *a, heap_t *b)
+{
+	int temp = a->n;
+
+	a->n = b->n;
+	b->n = temp;
 }
