13c1f7b3a2914d462a0a647ac8679536c84ea4ea
khalilBahi <7753@holbertonstudents.com>
2025-12-02 12:12:57 +0100

    a list of non-negative integers representing the heights of walls with unit width 1, as if viewing the cross-section of a relief map, calculate how many square units of water will be retained after it rains

---
+++ rain/0-rain.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Rain water trapping algorithm
+"""
+
+
+def rain(walls):
+    """
+    Calculate how many square units of water will be retained after it rains.
+    
+    Args:
+        walls: List of non-negative integers representing wall heights
+        
+    Returns:
+        Integer indicating total amount of rainwater retained
+    """
+    if not walls or len(walls) < 3:
+        return 0
+    
+    n = len(walls)
+    left = 0
+    right = n - 1
+    left_max = 0
+    right_max = 0
+    water = 0
+    
+    while left < right:
+        if walls[left] < walls[right]:
+            if walls[left] >= left_max:
+                left_max = walls[left]
+            else:
+                water += left_max - walls[left]
+            left += 1
+        else:
+            if walls[right] >= right_max:
+                right_max = walls[right]
+            else:
+                water += right_max - walls[right]
+            right -= 1
+    
+    return water
