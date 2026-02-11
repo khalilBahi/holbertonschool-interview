a88fa4f2f7ebe9d8077daea299c29ead10f33fcb
khalilBahi <7753@holbertonstudents.com>
2026-02-11 14:57:14 +0100

    Rotate 2D Matrix in-place by 90 degrees clockwise

--- rotate_2d_matrix/0-rotate_2d_matrix.py
+++ rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -4,23 +4,20 @@
 from typing import List
 
 
-def rotate_2d_matrix(matrix: List[List[int]]) -> None:
-	"""Rotate an n x n 2D matrix 90 degrees clockwise in-place.
+def rotate_2d_matrix(matrix):
+    """
+    Rotates an n x n 2D matrix 90 degrees clockwise in place.
 
-	Args:
-		matrix: Square matrix represented as a list of lists.
+    Args:
+        matrix (list of list of int): The matrix to rotate.
+    """
+    n = len(matrix)
 
-	The function modifies the matrix in-place and does not return anything.
-	"""
-	n = len(matrix)
-	if n == 0:
-		return
+    # Transpose the matrix (flip over the diagonal)
+    for i in range(n):
+        for j in range(i + 1, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
-	# Transpose the matrix in-place
-	for i in range(n):
-		for j in range(i + 1, n):
-			matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-
-	# Reverse each row to complete 90-degree clockwise rotation
-	for i in range(n):
-		matrix[i].reverse()
+    # Reverse each row
+    for row in matrix:
+        row.reverse()
